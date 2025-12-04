005ea1c5a74d4e10cfb8b896e17289b746521294
ashleec13 <527achen@frhsd.com>
2025-12-04 09:10:27 -0500

    small changes have been make and keep making the magic 8 ball

--- 8ball.js
+++ 8ball.js
@@ -7,6 +7,8 @@ function shakeMagic8Ball() {
         document.getElementById('response-text').innerText = 'Please Ask A Question';
         document.getElementById('response-text').classList = 'lead text-danger fw-bold';
         document.getElementById('response-img').src = 'loripsum.jpg';
+        document.getElementById('magic-button').classList.add('button-below-image');
+        return;
     }
 
     if (!question.trim()) {
@@ -22,47 +24,47 @@ function shakeMagic8Ball() {
 
     switch (randomNumber) {
         case 0:
-            answer = "Yes, definitely. Case 0";
+            answer = "Yes, definitely.";
             image = "images/8ball_yes_definitely.png";
             color = "text-success";
             break;
         case 1:
-            answer = "Ask again later. Case 1";
+            answer = "Ask again later.";
             image = "images/8ball_ask_again_later.png";
             color = "text-warning";
             break;
         case 2:
-            answer = "Don't count on it. Case 2";
+            answer = "Don't count on it.";
             image = "images/8ball_dont_count_on_it.png";
             color = "text-danger";
             break;
         case 3:
-            answer = "It is certain. Case 3";
+            answer = "It is certain.";
             image = "images/8ball_it_is_certain.png";
             color = "text-success";
             break;
         case 4:
-            answer = "My sources say no. Case 4";
+            answer = "My sources say no.";
             image = "images/8ball_my_sources_say_no.png";
             color = "text-danger";
             break;
         case 5:
-            answer = "Outlook good. Case 5";
+            answer = "Outlook good.";
             image = "images/8ball_outlook_good.png";
             color = "text-success";
             break;
         case 6:
-            answer = "Reply hazy, try again. Case 6";
+            answer = "Reply hazy, try again.";
             image = "images/8ball_reply_hazy_try_again.png";
             color = "text-warning";
             break;
         case 7:
-            answer = "Very doubtful. Case 7";
+            answer = "Very doubtful.";
             image = "images/8ball_very_doubtful.png";
             color = "text-danger";
             break;
         case 8:
-            answer = "Signs point to yes. Case 8";
+            answer = "Signs point to yes.";
             image = "images/8ball_signs_point_to_yes.png";
             color = "text-success";
             break;
@@ -70,4 +72,5 @@ function shakeMagic8Ball() {
     document.getElementById('response-text').innerText = answer;
     document.getElementById('response-text').classList = `display-4 text-center text-bold-outline ${color}`;
     document.getElementById('response-img').src = image;
+    document.getElementById('magic-button').classList.remove('button-below-image');
 }
